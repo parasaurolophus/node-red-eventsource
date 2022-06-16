7b0599df57c5fb8ee2aea12c32ddf90edd59fcfb
parasaurolophus <apps@rader.us>
2022-06-16 10:40:55 -0500

    Cosmetic and documentation improvements
    Make EventSource appearance more consistent with built in HTTP
    related nodes in Node-RED editor. Add labels to the input and
    output ports. Improve comments and inline documentation in
    JavaScript and example flows.

--- node-red-eventsource.js
+++ node-red-eventsource.js
@@ -1,13 +1,45 @@
+// Copyright 2022 Kirk Rader
+
+// See accompanying LICENSE file for open source usage permissions
+
+/**
+ * Wrap [eventsource](https://github.com/EventSource/eventsource) as a
+ * [Node-RED](https://nodered.org/) node
+ * 
+ * @param {*} RED Node-RED runtime API
+ */
 module.exports = function (RED) {
 
     const EventSource = require('eventsource')
+
+    /**
+     * Color names for `node.status({text: ... fill: ...})`
+     * 
+     * Note that the number and order of elements corresponds to the numeric
+     * values used for the `text` property.
+     */
     const statusFill = ['gray', 'yellow', 'green', 'red']
 
+    /**
+     * Node constructor
+     * 
+     * @param {*} config Node's runtime configuration
+     */
     function eventsource(config) {
 
         RED.nodes.createNode(this, config)
         var node = this
 
+        /**
+         * Called periodically to emit the current `eventsource.readyState`
+         * as the node's status
+         * 
+         * Status text is -1 if the wrapped `eventsource` does not exist.
+         * Otherwise, it is the value of `eventsource.readyState`.
+         * 
+         * As a side-effect, fire node.onclosed callback when
+         * `eventsource.readyState` reaches 2.
+         */
         function status() {
 
             const lastStatus = node.lastStatus || -2
@@ -34,15 +66,47 @@ module.exports = function (RED) {
             }
         }
 
-        function init() {
+        /**
+         * Create a new wrapped `eventsource` instance
+         * 
+         * @param {*} msg Message containing connection parameters 
+         */
+         function connect(msg) {
 
-            node.es = null
-            node.initDict = {}
-            node.lastStatus = -2
-            node.onclosed = null
+            node.url = msg.payload.url
+            node.initDict = msg.payload.initDict || {}
+            node.es = new EventSource(node.url, node.initDict)
+            status()
 
+            node.es.onopen = (evt) => {
+
+                node.send([null, { topic: 'open', payload: evt }, null])
+                status()
+
+            }
+
+            node.es.onerror = (err) => {
+
+                node.send([null, null, { topic: 'error', payload: err }])
+                status()
+
+            }
+
+            node.es.onmessage = (event) => {
+
+                node.send([{ topic: 'message', payload: event }, null, null])
+
+            }
         }
 
+        /**
+         * Node-RED life-cycle method called when the flow is being stopped or
+         * the node disabled.
+         * 
+         * @param {*} _removed Ignored
+         * @param {*} done Callback to inform the runtime asynchronously that
+         *                 the node is closed
+         */
         function close(_removed, done) {
 
             try {
@@ -64,47 +128,37 @@ module.exports = function (RED) {
             }
         }
 
-        function connect(msg) {
+        /**
+         * Handle a message sent to the node's input port
+         * 
+         * @param {*} msg The message to handle 
+         */
+        function handleMessage(msg) {
 
+            // clean up current connection, if any
             close(false, null)
 
+            // attempt to open a new connection if msg.payload is an object
+            // with a url property
             if ((typeof msg.payload) == 'object' && msg.payload.url !== undefined) {
 
-                node.url = msg.payload.url
-                node.initDict = msg.payload.initDict || {}
-                node.es = new EventSource(node.url, node.initDict)
-                status()
+                connect(msg)
 
-                node.es.onopen = (evt) => {
-
-                    node.send([null, { topic: 'open', payload: evt }, null])
-                    status()
-
-                }
-
-                node.es.onerror = (err) => {
-
-                    node.send([null, null, { topic: 'error', payload: err }])
-                    status()
-
-                }
-
-                node.es.onmessage = (event) => {
-
-                    node.send([{ topic: 'message', payload: event }, null, null])
-
-                }
             }
         }
 
-        init()
+        // initialize node to handle incoming messages
+        node.es = null
+        node.initDict = {}
+        node.lastStatus = -2
+        node.onclosed = null
         setInterval(status, 1000)
 
         node.on('close', close)
 
         node.on('input', function (msg) {
 
-            connect(msg)
+            handleMessage(msg)
 
         })
     }
